0799b8d07ea0b838533f94d09a520a4401656b70
leapkh <leapkh@yahoo.com>
2017-05-02 19:10:48 +0700

    - Add onCreate() to AboutActivity - Add Map activity

---
+++ app/src/main/java/kh/edu/rupp/ckcc/ckcc/activity/MapActivity.java
@@ -0,0 +1,11 @@
+package kh.edu.rupp.ckcc.ckcc.activity;
+
+import android.app.Activity;
+
+/**
+ * CKCC-Android-App
+ * Created by leapkh on 5/2/17.
+ */
+
+public class MapActivity extends Activity {
+}
